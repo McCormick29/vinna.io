51f32995f7219930156e61c424ebff6b160cfc65
StevenCreates <steven@stevencreates.tech>
2021-12-07 18:21:55 -0700

    more formatting

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
-import {AccountTokens} from './components/AccountTokens'
+import React from "react";
+import styles from "../styles/Home.module.css";
+import { AccountTokens } from "./components/AccountTokens";
 
 export default function Home() {
-
   return (
     <div className={styles.container}>
       <AccountTokens />
     </div>
-  )
+  );
 }
