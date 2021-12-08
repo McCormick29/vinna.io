28f9088173b816cf588982ca542b5b8febd49625
StevenCreates <steven@stevencreates.tech>
2021-12-07 18:21:26 -0700

    formatted syntax account tokens component

--- pages/components/AccountTokens.js
+++ pages/components/AccountTokens.js
@@ -1,47 +1,56 @@
-import React from 'react'
+import React from "react";
 // import { getAccountTokenBalance } from '../utils/eth_api';
-import { getAccountTokenTransfers, getAccountTokenHolds } from '../utils/eth_api';
-import { AmountOfToken } from './AmountOfToken';
+import {
+  getAccountTokenTransfers,
+  getAccountTokenHolds,
+} from "../utils/eth_api";
+import { AmountOfToken } from "./AmountOfToken";
 
-export const AccountTokens = ({tokenAddress = '0xb70ac54e863d8F173e0C0EA9fE2848831bFDfA5B'}) => {
-    const tokenArr = getAccountTokenTransfers(tokenAddress)
-    console.log(tokenArr)
-    const [tokens, setTokens] = React.useState(undefined)
+export const AccountTokens = ({
+  tokenAddress = "0xb70ac54e863d8F173e0C0EA9fE2848831bFDfA5B",
+}) => {
+  const tokenArr = getAccountTokenTransfers(tokenAddress);
+  console.log(tokenArr);
+  const [tokens, setTokens] = React.useState(undefined);
 
-    const getTokenRows = (array) => {
-        const tokens = array?.map((token) => ({
-            tokenName: token.tokenName,
-            tokenAddress: token.contractAddress,
-        }))
-        const uniqueTokens = [...new Map(tokens.map(v => [v.tokenAddress, v])).values()]
+  const getTokenRows = (array) => {
+    const tokens = array?.map((token) => ({
+      tokenName: token.tokenName,
+      tokenAddress: token.contractAddress,
+    }));
+    const uniqueTokens = [
+      ...new Map(tokens.map((v) => [v.tokenAddress, v])).values(),
+    ];
 
-        setTokens(uniqueTokens)
-    }
+    setTokens(uniqueTokens);
+  };
 
-    React.useEffect(() => {
-        if(tokenArr === undefined){
-            return null
-        } else(
-            getTokenRows(tokenArr)
-        )
-    }, [tokenArr])
+  React.useEffect(() => {
+    if (tokenArr === undefined) {
+      return null;
+    } else getTokenRows(tokenArr);
+  }, [tokenArr]);
 
-
-    return (
+  return (
+    <div>
+      Accounts Unique Tokens
+      {tokens !== undefined ? (
         <div>
-            Accounts Unique Tokens
-            {tokens !== undefined ? (
+          {tokens?.map((token) => (
             <div>
-            {tokens?.map((token) => (
-                <div> 
-                    <div>Token: {token.tokenName}</div>
-                    <div>Contract: {token.tokenAddress}</div>
-                    <AmountOfToken account={tokenAddress} token={token.tokenAddress} />
-                    {/* <div>Amount: {getAccountTokenHolds(tokenAddress, token.tokenAddress)}</div> */}
-                </div>
-            ))}
+              <div>Token: {token.tokenName}</div>
+              <div>Contract: {token.tokenAddress}</div>
+              <AmountOfToken
+                account={tokenAddress}
+                token={token.tokenAddress}
+              />
+              {/* <div>Amount: {getAccountTokenHolds(tokenAddress, token.tokenAddress)}</div> */}
             </div>
-            ) : <div> no transfers </div> }
+          ))}
         </div>
-    )
-}
+      ) : (
+        <div> no transfers </div>
+      )}
+    </div>
+  );
+};
