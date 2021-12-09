2b2232a8b2bbd63943877f136f4ab9092d237259
StevenCreates <steven@stevencreates.tech>
2021-12-09 11:59:41 -0700

    added storybook

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Link from 'next/link'
+
 import { AccountTokens } from "./components/TokenList";
 
 export default function Home() {
   return (
     <div className='px-4'>
-      <AccountTokens />
+
     </div>
   );
 }
