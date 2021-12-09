82c14551ab43869dfc8b15f1b777d2735e462c49
StevenCreates <steven@stevencreates.tech>
2021-12-09 14:10:34 -0700

    started landing page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,194 @@
 import React from "react";
-import Link from 'next/link'
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronRightIcon } from "@heroicons/react/solid";
+
+const navigation = [
+  { name: 'Vinna.io', href: '#' },
+
+]
 
 export default function Home() {
   return (
-    <div className='px-4'>
+    <div className="bg-zinc-900 h-screen">
+      <div className="relative overflow-hidden">
+        <Popover as="header" className="relative">
+          <div className="bg-zinc-900 h-full pt-6">
+            <nav
+              className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-1">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      className="h-8 w-auto sm:h-10"
+                      // src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="hidden space-x-8 md:flex md:ml-10">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-base font-medium text-white hover:text-gray-300"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:space-x-6">
+                {/* <a
+                  href="#"
+                  className="text-base font-medium text-white hover:text-gray-300"
+                >
+                  Log in
+                </a> */}
+                <a
+                  href="#"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+                >
+                  Coming Soon
+                </a>
+              </div>
+            </nav>
+          </div>
 
+          <Transition
+            as={React.Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+            >
+              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="px-5 pt-4 flex items-center justify-between">
+                  <div>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="pt-5 pb-6">
+                  <div className="px-2 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="mt-6 px-5">
+                    <a
+                      href="#"
+                      className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                    >
+                      Start free trial
+                    </a>
+                  </div>
+                  <div className="mt-6 px-5">
+                    <p className="text-center text-base font-medium text-gray-500">
+                      Existing customer?{" "}
+                      <a href="#" className="text-gray-900 hover:underline">
+                        Login
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+        <main>
+          <div className="pt-10 bg-zinc-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+            <div className="mx-auto max-w-7xl lg:px-8">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                  <div className="lg:py-24">
+                    <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                      <span className="block">Crypto Kunnusta <span className='italic text-sm'>(knowledge)</span> is</span>
+                    </h1>
+                    <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+                      how you Vinna! We believe that knowledge is power by providing data to find trends, track whales in a way never seen before!
+                    </p>
+                    <div className="mt-10 sm:mt-12">
+                      <form
+                        action="#"
+                        className="sm:max-w-xl sm:mx-auto lg:mx-0"
+                      >
+                        <div className="sm:flex">
+                          <div className="min-w-0 flex-1">
+                            <label htmlFor="email" className="sr-only">
+                              Email address
+                            </label>
+                            <input
+                              id="email"
+                              type="email"
+                              placeholder="Enter your email"
+                              className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                            />
+                          </div>
+                          <div className="mt-3 sm:mt-0 sm:ml-3">
+                            <button
+                              type="submit"
+                              className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                            >
+                              Join Waiting List
+                            </button>
+                          </div>
+                        </div>
+                        <p className="mt-3 text-sm pr-4 text-gray-300 sm:mt-4">
+                          Vinna is a work in progress, timelines will be announced soon! Subscribe or follow our twitter for updates!
+                        </p>
+                        <a href="#" className="font-medium text-white">
+                            terms of service
+                          </a>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+                  <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                    {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
+                    <img
+                      className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                      src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
